76252af5406e9133f0ead70acb721c4ba102ecb7
Jakub Serweta <44199012+jserweta@users.noreply.github.com>
2023-05-17 21:43:15 +0100

    update constants/api.ts

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,4 +1,4 @@
 const API_URL = "https://www.alphavantage.co/query?";
-const API_KEY = "QYOWP5SXIHB6BV3X"; //YCW6YFXYXAVBV19L
+const API_KEY = "YCW6YFXYXAVBV19L"; //QYOWP5SXIHB6BV3X
 
 export { API_URL, API_KEY };
